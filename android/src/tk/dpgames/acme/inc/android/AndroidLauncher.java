299024786b6cc76da47320ea60d69d39a4ebfb18
BrandonDyer64 <brandondyer64@gmail.com>
2015-07-31 16:04:10 -0400

    Initial build

---
+++ android/src/tk/dpgames/acme/inc/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package tk.dpgames.acme.inc.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import tk.dpgames.acme.inc.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
---
+++ desktop/src/tk/dpgames/acme/inc/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tk.dpgames.acme.inc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tk.dpgames.acme.inc.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
