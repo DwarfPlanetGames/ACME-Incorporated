25e0077e55a398d3c63f193b26df4c60307a4d33
BrandonDyer64 <brandondyer64@gmail.com>
2015-08-03 00:11:11 -0400

    Signed-off-by: BrandonDyer64 <brandondyer64@gmail.com>

--- core/src/tk/dpgames/acme/inc/voxes/Vox.java
+++ core/src/tk/dpgames/acme/inc/voxes/Vox.java
@@ -1,5 +1,22 @@
 package tk.dpgames.acme.inc.voxes;
 
-public class Vox {
-
+public abstract class Vox {
+	
+	String data = "";
+	public int texX = 0, texY = 0;
+	public boolean canMine = true;
+	public float temper = 1f; // 1 would be dirt, 2 would be rock
+	
+	public Vox(int texX, int texY, String data) {
+		this.data = data;
+		this.texX = texX;
+		this.texY = texY;
+	}
+	
+	public void click(float distance){};
+	
+	public void mined(){};
+	
+	public void placed(){};
+	
 }
